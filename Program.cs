c9ff409609945ebbcdd318cbc5726af011c948be
grimroot23 <nick.dupe21860@gmail.com>
2022-09-22 15:50:25 -0600

    testing nick

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-<<<<<<< HEAD
-Console.WriteLine("Hi Guys.");
-=======
 Console.WriteLine("Clayton was here!");
->>>>>>> b07e2c5c55feebf2e4c51c7db94d84b9a952bc1c
+
+
+
+
+
+
